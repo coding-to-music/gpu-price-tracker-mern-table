e20d3fe3e8833f64b41d1193d5bb69baca73e6e8
Jayden-Chiu <jaydenchiu@rogers.com>
2020-12-22 04:55:46 -0500

    added gpu images to table

--- client/src/components/columns.js
+++ client/src/components/columns.js
@@ -1,22 +1,29 @@
 export const COLUMNS = [
-  {
-    Header: 'Image',
-    accessor: 'img'
-  },
-  {
-    Header: 'Name',
-    accessor: 'title'
-  },
-  {
-    Header: 'Price',
-    accessor: 'price'
-  },
-  {
-    Header: 'Brand',
-    accessor: 'brand'
-  },
-  {
-    Header: 'Retailer',
-    accessor: 'retailer'
-  },
-]
+	{
+		Header: 'Image',
+		accessor: 'img',
+		Cell: (row) => {
+			return (
+				<div>
+					<img height={100} src={row.row.original.img} />
+				</div>
+			);
+		},
+	},
+	{
+		Header: 'Name',
+		accessor: 'title',
+	},
+	{
+		Header: 'Price',
+		accessor: 'price',
+	},
+	{
+		Header: 'Brand',
+		accessor: 'brand',
+	},
+	{
+		Header: 'Retailer',
+		accessor: 'retailer',
+	},
+];
