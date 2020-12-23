bb4f24a39b957fffa60910809a85301d99e3cece
Jayden-Chiu <jaydenchiu@rogers.com>
2020-12-23 00:40:58 -0500

    fixed .env path

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+require('dotenv').config({ path: '../.env'});
 
 const PORT = process.env.PORT || 5000;
 const app = express();
