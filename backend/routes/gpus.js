9f0a5c455f8f6378c6e1e1bd36d613473c5bad35
Jayden-Chiu <jaydenchiu@rogers.com>
2021-02-16 07:06:13 -0500

    added delete route

--- backend/routes/gpus.js
+++ backend/routes/gpus.js
@@ -30,4 +30,27 @@ router.get(
 	}
 );
 
+router.delete(
+	'/delete',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		var saved = req.user.saved;
+		const filtered = saved.filter((val) => val !== req.body.id);
+
+		if (saved.length == filtered.length)
+			res
+				.status(406)
+				.json({ messaage: 'Could not find GPU in saved', id: null });
+		else
+			req.user.save((err) => {
+				if (err)
+					res.status(500).json({ message: 'An error occurred', id: null });
+				else
+					res
+						.status(200)
+						.json({ message: 'Successfully deleted', id: req.body.id });
+			});
+	}
+);
+
 module.exports = router;
