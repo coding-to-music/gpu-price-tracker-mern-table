a9e554f0d4e207621caf71fd74ee84008dc7bf57
coding-to-music <connors.tom@gmail.com>
2022-04-17 07:05:59 +0000

    Heroku

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,23 +1,27 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema, model } = require("mongoose");
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	username: {
-		type: String,
-		lowercase: true,
-		required: true,
-		index: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	saved: [{ type: String }],
+  username: {
+    type: String,
+    lowercase: true,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  saved: [{ type: String }],
 });
 
-const userDB = mongoose.connection.useDb('users');
+const User = model("User", userSchema);
 
-const User = userDB.model('user', userSchema);
+// const userDB = mongoose.connection.useDb("users");
+
+// const User = userDB.model("user", userSchema);
 
 module.exports = User;
