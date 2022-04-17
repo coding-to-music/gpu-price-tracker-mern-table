ca48e9014a69c5fa09587727e5b13305e9059ed5
coding-to-music <connors.tom@gmail.com>
2022-04-17 05:41:30 +0000

    Heroku

--- backend/scrapers/index.js
+++ backend/scrapers/index.js
@@ -1,21 +1,22 @@
-const scrapeNewegg = require('./newegg');
-const scrapeMemoryExpress = require('./memoryexpress');
-const scrapeBestBuy = require('./bestbuy');
-const schedule = require('node-schedule');
+const scrapeNewegg = require("./newegg");
+const scrapeMemoryExpress = require("./memoryexpress");
+const scrapeBestBuy = require("./bestbuy");
+const schedule = require("node-schedule");
 
-require('dotenv').config({ path: '../../.env' });
+require("dotenv").config({ path: "../../.env" });
 
 const scrapeAll = () => {
-	scrapeNewegg();
-	scrapeBestBuy();
-	scrapeMemoryExpress();
+  scrapeNewegg();
+  scrapeBestBuy();
+  scrapeMemoryExpress();
 };
 
-if (process.env.NODE_ENV === 'production') {
-	// scrapeAll();
+if (process.env.NODE_ENV === "production") {
+  // scrapeAll();
+  schedule.scheduleJob("*/60 * * * *", scrapeAll);
 } else {
-	// scrapeAll();
-	schedule.scheduleJob('*/60 * * * *', scrapeAll);
+  // scrapeAll();
+  schedule.scheduleJob("*/60 * * * *", scrapeAll);
 }
 
 module.exports = scrapeAll;
