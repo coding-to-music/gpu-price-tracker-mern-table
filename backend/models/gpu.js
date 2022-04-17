32565942c73cb124bcc8e4b909ffe17572de7f93
coding-to-music <connors.tom@gmail.com>
2022-04-17 07:26:43 +0000

    Heroku

--- backend/models/gpu.js
+++ backend/models/gpu.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema, model } = require("mongoose");
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
 const gpuSchema = new Schema(
-	{
-		title: {
-			type: String,
-			required: true,
-		},
-		brand: String,
-		retailer: String,
-		price: Schema.Types.Mixed,
-		link: String,
-		img: String,
-		Model: String,
-	},
-	{ timestamps: true }
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    brand: String,
+    retailer: String,
+    price: Schema.Types.Mixed,
+    link: String,
+    img: String,
+    Model: String,
+  },
+  { timestamps: true }
 );
 
-const gpuDB = mongoose.connection.useDb('products');
+const Gpu = model("Gpu", gpuSchema);
 
-const Gpu = gpuDB.model('Gpu', gpuSchema);
+// const gpuDB = mongoose.connection.useDb('products');
+
+// const Gpu = gpuDB.model('Gpu', gpuSchema);
 
 module.exports = Gpu;
