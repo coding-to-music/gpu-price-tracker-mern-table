3f432f9ce1d62b0608b5c341f16f5268d4555f2f
coding-to-music <connors.tom@gmail.com>
2022-04-17 01:41:58 +0000

    Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const routes = require('./routes/index');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const routes = require("./routes/index");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-require('./auth/auth');
+require("./auth/auth");
 
-require('dotenv').config({ path: '../.env' });
+require("dotenv").config({ path: "../.env" });
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -17,36 +17,35 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // Connect to DB
-const URI = process.env.ATLAS_URI;
+const URI = process.env.MONGODB_URI;
 mongoose.connect(URI, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
-db.once('open', () => {
-	console.log('Connected to MongoDB datebase!');
+db.once("open", () => {
+  console.log("Connected to MongoDB datebase!");
 });
 
 // API routes
 app.use(routes);
 
 // Routing for heroku
-if (process.env.NODE_ENV === 'production') {
-	// Serve any static files
-	app.use(express.static(path.join(__dirname, '../client/build')));
-	// Handle React routing, return all requests to React app
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-	});
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
 }
 
 // start server
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
